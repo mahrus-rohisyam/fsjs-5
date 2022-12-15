1f2837129625b16a3b50b16d1947df192d710673
mahrus-rohisyam <mahrusaim@gmail.com>
2022-12-16 06:19:02 +0700

    Adding router ./api

--- node-express/Server.js
+++ node-express/Server.js
@@ -1,5 +1,4 @@
 require("dotenv").config();
-const { application } = require("express");
 const express = require("express");
 const app = express();
 const path = require("path");
@@ -11,9 +10,21 @@ app.use(express.static(__dirname));
 // Serving multiple form
 app.use(express.urlencoded({ extended: false }));
 
+// Body parser
+app.use(express.json());
+
 // Router
+// localhost:8000/
 app.use("/", require("./router/root"));
+// localhost:8000/subdir/
 app.use("/subdir", require("./router/subdir"))
+// localhost:8000/user/
+app.use("/api", require("./router/api"));
+
+app.use((req, res) => {
+  res.status(404);
+  res.sendFile(path.join(__dirname, "views", "404.html"));
+});
 
 app.listen(PORT, () => {
   console.log("Server is running on http://localhost:" + PORT);
